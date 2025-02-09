8bfe773c2d9b4a4670a89b6d47c3bce88ea29f7e
Mikhail Mogilnikov <mikhail.mogilnikov02@gmail.com>
2025-02-09 23:43:55 +0300

    refactor: Simplify Popover component and remove Squircle dependency
    - Remove Squircle wrapper from PopoverContent
    - Update text color class to use generic foreground token
    - Add minimum width to popover content

--- apps/website/src/global/ui/popover/popover.tsx
+++ apps/website/src/global/ui/popover/popover.tsx
@@ -4,7 +4,6 @@ import {
   PopoverContentProps,
   PopoverContent as PopoverContentPrimitive,
 } from "@radix-ui/react-popover";
-import { Squircle } from "@blur-ui/squircle";
 
 import { cn } from "../../lib/utils/styling/cn";
 
@@ -12,17 +11,15 @@ export const PopoverContent = (props: PopoverContentProps) => {
   const { className, sideOffset = 10, ...rest } = props;
 
   const popoverContentClassNames = cn(
-    "text-content3-foreground z-10000 flex w-fit flex-col gap-1 rounded-lg bg-default p-2 shadow-md outline-none data-[side='bottom']:origin-top data-[side='top']:origin-bottom data-[state=closed]:animate-popover-out data-[state=open]:animate-popover-in",
+    "z-10000 flex w-fit min-w-36 flex-col gap-1 rounded-lg bg-default p-2 text-foreground shadow-md outline-none data-[side='bottom']:origin-top data-[side='top']:origin-bottom data-[state=closed]:animate-popover-out data-[state=open]:animate-popover-in",
     className,
   );
 
   return (
-    <Squircle asChild>
-      <PopoverContentPrimitive
-        className={popoverContentClassNames}
-        sideOffset={sideOffset}
-        {...rest}
-      />
-    </Squircle>
+    <PopoverContentPrimitive
+      className={popoverContentClassNames}
+      sideOffset={sideOffset}
+      {...rest}
+    />
   );
 };
